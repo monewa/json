34ec014371d08e5a6c02a9cf4997dec27feca2ea
monewa <107672359+monewa@users.noreply.github.com>
2022-08-23 08:40:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(process.env.port, () => {
+server.listen(process.env.PORT, () => {
   console.log('JSON Server is running')
 })
